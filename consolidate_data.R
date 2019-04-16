5a40ebefa938e786e32c958abbb63514cd5afc77
alcantarar <ryan.alcantara@colorado.edu>
2019-04-16 01:39:50 -0600

    r code to tidy data

---
+++ consolidate_data.R
@@ -0,0 +1,24 @@
+data <- read.csv('/Users/ryanalcantara/Professional Drive/Subreddit/code_repo/literature_update/RYANDATA_no_PoW.csv', stringsAsFactors = F)
+
+data.complete <- data[(complete.cases(data$topic)),]
+nrow(data.complete)
+unique(data.complete$topic)
+
+data.complete$topic[grepl('ORTHO',data.complete$topic)] <- 'ORTHOPAEDICS'
+data.complete$topic[grepl('ANIMAL',data.complete$topic)] <- 'COMPARATIVE'
+data.complete$topic[grepl('COMPARATIVE',data.complete$topic)] <- 'COMPARATIVE'
+data.complete$topic[grepl('VETERINARY',data.complete$topic)] <- 'COMPARATIVE'
+data.complete$topic[grepl('BIOMECHANICS IN EDUCATION',data.complete$topic)] <- NA
+data.complete$topic[grepl('DIGESTIVE',data.complete$topic)] <- NA
+data.complete$topic[grepl('WHEELCHAIR',data.complete$topic)] <- NA
+data.complete$topic[grepl('OBSTETRICS',data.complete$topic)] <- NA
+data.complete$topic[grepl('REACHING',data.complete$topic)] <- NA
+data.complete$topic[grepl('POSTURE',data.complete$topic)] <- NA
+data.complete$topic[grepl('HAND',data.complete$topic)] <- NA
+data.complete$topic[grepl('BOTANY',data.complete$topic)] <- NA
+data.complete$topic[grepl('TRAUMA',data.complete$topic)] <- NA
+
+table(data.complete$topic)
+data.complete <- data.complete[(complete.cases(data.complete$topic)),]
+nrow(data.complete)
+write.csv(data.complete, file = 'RYANDATA_consolidated.csv')
