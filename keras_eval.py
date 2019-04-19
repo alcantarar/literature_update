111528c0d1c18364af33463f8d924ea6e0b2f37a
GBruening <garrickbruening@gmail.com>
2019-04-19 12:28:25 -0600

    Cleaned up the code, added some comments

--- keras_eval.py
+++ keras_eval.py
@@ -1,11 +1,14 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 19 10:24:56 2019
+Runs the CNN created in keras_1.py on a data set.
+Currently it's set to run on the 'text' variable, which is a list of strings.
+
+Needs to load the model: model.json
+    label encoder:       LabelEncoder.npy
+    and vectorizer:      Vectorizer.pkl
 
 @author: Gary
 """
-
-
 #========================= Load the Model =====================================
 from keras.models import model_from_json
 # load json and create model
@@ -19,16 +22,6 @@ model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accurac
 print("Loaded model from disk")
  
 #========================= Eval on test =======================================
-#score = loaded_model.evaluate(X_test, y_test, verbose=0)
-#print("%s: %.2f%%" % (loaded_model.metrics_names[1], score[1]*100))
-
-#text = 'neuromechanical effort proxies estimation computational'
-#text = text.lower()
-
-#test = pd.DataFrame(data = {'title': [text]})
-#vectors = vectorizer.fit_transform(test['title'])
-#vectors.shape
-
 import pickle
 import numpy as np
 import pandas as pd
@@ -38,17 +31,12 @@ vect = pickle.load(open('Vectorizer.pkl','rb'))
 le = LabelEncoder()
 le.classes_   = np.load('LabelEncoder.npy')
 print('Loaded Vectorizer')
+
 text = ['neuromechanical effort, + = proxies estimation computational',
         'testing this other thing']
+
 text = [text.lower() for text in text]
 test = pd.DataFrame(data = {'title': text})
 test = vect.transform(test['title'])
 prediction_val = model.predict(test)
 topics2 = [le.inverse_transform([np.argmax(top_val)])[0] for top_val in model.predict(test)]
-
-#s = (vectorizer.transform(list(text)))
-#print (s.shape)
-#d = (loaded_model.predict(s))
-#perc = np.exp(MNB.predict_log_proba(s)[0])
-#le.inverse_transform(d)[0]
-#print('Naive Bayes Accuracy: ' + '%.2f' % metrics.accuracy_score(y_test, MNB_pred) + '%')
